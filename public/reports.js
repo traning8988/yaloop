0bc5845dd1a2046e59f9bec391cf0501fe55a987
mako-agawa <mako.agawa@gmail.com>
2024-09-02 21:50:25 +0900

    "commit"

---
+++ public/reports.js
@@ -0,0 +1,15 @@
+document.getElementById("loadData").addEventListener("click", function () {
+  fetch("/data")
+    .then((response) => response.json())
+    .then((data) => {
+      const content = document.getElementById("content");
+      content.innerHTML = "";
+
+      // dataはオブジェクトなので、直接プロパティにアクセスします
+      const p = document.createElement("p");
+      p.className = "report-totaltime"; // クラス名を設定
+      p.textContent = `学習時間 ${data.duration_hour} 時間 ${data.duration_minute} 分`;
+      content.appendChild(p);
+    })
+    .catch((error) => console.error("Error loading data:", error));
+});
