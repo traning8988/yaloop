3ad6a25d62881de04c759448735dd7377e8b2f93
mako-agawa <mako.agawa@gmail.com>
2024-09-02 22:34:17 +0900

    reports.jsを追加

---
+++ public/reports.js
@@ -0,0 +1,33 @@
+
+document.getElementById("btn").addEventListener("click", function (event) {
+  event.preventDefault();  // フォームのデフォルトの送信を防ぐ
+
+  // 入力フィールドからタスクを取得
+  const taskInput = document.querySelector('.task-input').value;
+
+  if (taskInput) {
+     // 新しいリストアイテムを作成
+     const li = document.createElement('li');
+     li.classList.add("report-task");
+     
+     // タスクタイトルの<p>を作成
+     const taskTitle = document.createElement('p');
+     taskTitle.classList.add("task-title");
+     taskTitle.textContent = taskInput;
+
+     const taskTime = document.createElement('p');
+     taskTime.classList.add("task-time");
+     taskTime.textContent = "now-working";
+     
+     // <p>を<li>に追加
+     li.appendChild(taskTitle);
+     li.appendChild(taskTime);
+     
+     // <li>を<ul>に追加
+     document.querySelector('.report-tasks').appendChild(li);
+
+     // 入力フィールドをクリア
+     document.querySelector('.task-input').value = '';
+  }
+});
+
