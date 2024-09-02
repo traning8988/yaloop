fe64cbe1ab6b3f23c5adfe9f71732e221af89761
haruki1227041209 <haruki122704@gmail.com>
2024-09-02 14:14:21 +0900

    クリックで学習時間を表示

--- public/script.js
+++ public/script.js
@@ -7,7 +7,9 @@ document.getElementById("loadData").addEventListener("click", function () {
 
       data.forEach((item) => {
         const p = document.createElement("p");
-        p.textContent = `${item.id}: ${item.name} - ${item.description}`;
+        p.className = "report-totaltime"; // クラス名を設定
+        // p.textContent = `${item.id}: ${item.name} - ${item.description} - ${item.duration}`;
+        p.textContent = `学習時間: ${item.duration_hour}時間${item.duration_minute}分`;
         content.appendChild(p);
       });
     })
--- server.rb
+++ server.rb
@@ -2,15 +2,19 @@ require 'webrick'
 require 'json'
 
 sample_data = [
-  { id: 1, name: 'Item 1', description: 'This is the first item' },
-  { id: 2, name: 'Item 2', description: 'This is the second item' },
-  { id: 3, name: 'Item 3', description: 'This is the third item' }
+  { id: 1, name: 'Item 1', description: 'This is the first item', duration: '01:00:00' },
+  { id: 2, name: 'Item 2', description: 'This is the second item', duration: '02:00:00' },
+  { id: 3, name: 'Item 3', description: 'This is the third item', duration: '03:00:00' }
+]
+
+duration = [
+  duration_hour: "02", duration_minute: "30", duration_second: "30"
 ]
 
 server = WEBrick::HTTPServer.new(:Port => 8000, :DocumentRoot => './public')
 
 server.mount_proc '/data' do |req, res|
-  res.body = sample_data.to_json
+  res.body = duration.to_json
   res['Content-Type'] = 'application/json'
 end
 
