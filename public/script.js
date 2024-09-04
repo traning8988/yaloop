8e3e14e669acddcbde04fbce4544d2a5d9c9f53b
haruki1227041209 <haruki122704@gmail.com>
2024-09-04 22:23:59 +0900

    ユーザーネームの出力

--- public/script.js
+++ public/script.js
@@ -18,23 +18,23 @@ function fetchDataAndUpdate() {
       const taskContainer = document.getElementById("report-tasks");
       if (taskContainer) {
         taskContainer.innerHTML = "";
-    
+
         // タスクのリストを生成
         data.tasks.forEach((task) => {
           const taskDiv = document.createElement("div");
           taskDiv.className = "report-task oddtask-color"; // タスクコンテナのクラス名
-    
+
           const taskTitle = document.createElement("p");
           taskTitle.className = "task-title "; // タスクタイトルのクラス名
           taskTitle.textContent = task.title;
-    
+
           const taskTime = document.createElement("p");
           taskTime.className = "task-time"; // タスク時間のクラス名
           taskTime.textContent = task.time;
-    
+
           taskDiv.appendChild(taskTitle);
           taskDiv.appendChild(taskTime);
-    
+
           taskContainer.appendChild(taskDiv);
         });
       }
@@ -48,6 +48,17 @@ function fetchDataAndUpdate() {
         reportParagraph.className = ""; // クラス名を設定
         reportParagraph.textContent = `日報\n ${data.description}`;
         reportContent.appendChild(reportParagraph);
+      }
+
+      const userContent = document.getElementById("user-name");
+      if (userContent) {
+        userContent.innerHTML = "";
+
+        // 学習時間を表示する要素を作成
+        const userParagraph = document.createElement("p");
+        userParagraph.className = ""; // クラス名を設定
+        userParagraph.textContent = `${data.user.name}`;
+        userContent.appendChild(userParagraph);
       }
     })
     .catch((error) => console.error("Error loading data:", error));
