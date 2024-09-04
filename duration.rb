c4eeca5976f4ec2dae25f08f79de86e9473aa6ec
haruki1227041209 <haruki122704@gmail.com>
2024-09-04 10:31:26 +0900

    レポートがnil、空でも動くように

--- duration.rb
+++ duration.rb
@@ -77,4 +77,4 @@ sample_study_datas = fetch_times_data_today
 sample_study_duration_datas(sample_study_datas) unless sample_study_datas.nil? || sample_study_datas.empty?
 
 report_today = fetch_report_today
-daily_report(report_today)
+daily_report(report_today) unless report_today.nil? || report_today.empty?
