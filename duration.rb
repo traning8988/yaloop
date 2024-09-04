af6f4e98a237857ac5803c29231f548bba4427c1
haruki1227041209 <haruki122704@gmail.com>
2024-09-04 14:26:33 +0900

    /dataに送るデータを作成

--- duration.rb
+++ duration.rb
@@ -1,12 +1,11 @@
 require 'time'
 require_relative 'fetch_times_table'
 
-$duration = {}
-
 def sample_study_duration_datas(sample_study_datas)
   daily_study_durations = {}
   total_task_study_durations = {}
   daily_task_study_durations = {}
+  $duration = {} # 1日のデータを全て入れる
 
   sample_study_datas.each do |sample_study_data|
     start_time = Time.parse(sample_study_data[:start_time])
@@ -33,10 +32,18 @@ def sample_study_duration_datas(sample_study_datas)
     puts "学習時間: #{seconds_to_hms(total_seconds)}"
   end
 
+  tasks_array = []
   puts "--- やったこと ---"
   total_task_study_durations.each do |task_title, total_seconds|
+    task_hash = {
+      title: task_title,
+      time: seconds_to_hms_a(total_seconds)
+    }
+    tasks_array << task_hash
+
     puts "#{task_title} : #{seconds_to_hms(total_seconds)}時間"
   end
+
 
 =begin
   daily_task_study_durations.each do |date, task_titles|
@@ -49,12 +56,13 @@ def sample_study_duration_datas(sample_study_datas)
     p total_task_study_durations
     p daily_task_study_durations
 =end
-
-  hours, minutes, seconds = seconds_to_hms(daily_study_durations.values.first).split(":")
+  hours, minutes, seconds = seconds_to_hms(daily_study_durations.values.first).split(":") # 1日の学習時間
+  
   $duration = {
     duration_hour: hours,
     duration_minute: minutes,
-    duration_second: seconds
+    duration_second: seconds,
+    tasks: tasks_array
   }
 end
 
@@ -68,6 +76,15 @@ def seconds_to_hms(seconds)
   format("%02d:%02d:%02d", hours, minutes, secs)
 end
 
+def seconds_to_hms_a(seconds)
+  return if seconds.nil?
+
+  hours = seconds / 3600
+  minutes = (seconds % 3600) / 60
+
+  format("%d時間%d分", hours, minutes)
+end
+
 def daily_report(daily_report_data)
   puts "良かったこと"
   puts daily_report_data[0][:description]
@@ -77,4 +94,9 @@ sample_study_datas = fetch_times_data_today
 sample_study_duration_datas(sample_study_datas) unless sample_study_datas.nil? || sample_study_datas.empty?
 
 report_today = fetch_report_today
+$duration.merge(report_today[0]) # 日報をdurationに追加
 daily_report(report_today) unless report_today.nil? || report_today.empty?
+
+  # $duration[:tasks].each do |task|
+  #   puts "#{task[:title]} は #{task[:time]} です"
+  # end
