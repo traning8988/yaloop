7cf1d194819617ef83e1edf26036a2b29ba1aad3
mako-agawa <mako.agawa@gmail.com>
2024-09-06 07:39:38 +0900

    commit

---
+++ public/idChange.js
@@ -0,0 +1,53 @@
+const data = {
+    "today": {
+      "duration_hour": "06",
+      "duration_minute": "30",
+      "duration_second": "07",
+      "tasks": [
+        {"title": "Ruby", "hours": "01", "minutes": "15", "seconds": "00"},
+        {"title": "チームミーティング", "hours": "02", "minutes": "05", "seconds": "00"},
+        {"title": "JavaScript", "hours": "01", "minutes": "05", "seconds": "00"},
+        {"title": "React", "hours": "02", "minutes": "05", "seconds": "00"},
+        {"title": "HTML", "hours": "00", "minutes": "00", "seconds": "07"}
+      ],
+      "user": {
+        "id": 1,
+        "name": "阿川",
+        "email": "agawa@example.com"
+      }
+    },
+    "yesterday": {
+      "duration_hour": "09",
+      "duration_minute": "30",
+      "duration_second": "07",
+      "tasks": [
+        {"title": "Ruby", "hours": "03", "minutes": "15", "seconds": "00"},
+        {"title": "チームミーティング", "hours": "01", "minutes": "05", "seconds": "00"},
+        {"title": "JavaScript", "hours": "01", "minutes": "05", "seconds": "00"},
+        {"title": "React", "hours": "02", "minutes": "05", "seconds": "00"},
+        {"title": "HTML", "hours": "02", "minutes": "00", "seconds": "07"}
+      ],
+      "user": {
+        "id": 1,
+        "name": "阿川",
+        "email": "agawa@example.com"
+      }
+    },
+    "day_before_yesterday": {
+      "duration_hour": "08",
+      "duration_minute": "30",
+      "duration_second": "07",
+      "tasks": [
+        {"title": "Ruby", "hours": "02", "minutes": "15", "seconds": "00"},
+        {"title": "チームミーティング", "hours": "02", "minutes": "05", "seconds": "00"},
+        {"title": "JavaScript", "hours": "02", "minutes": "05", "seconds": "00"},
+        {"title": "React", "hours": "01", "minutes": "05", "seconds": "00"},
+        {"title": "HTML", "hours": "01", "minutes": "00", "seconds": "07"}
+      ],
+      "user": {
+        "id": 1,
+        "name": "阿川",
+        "email": "agawa@example.com"
+      }
+    }
+  };
