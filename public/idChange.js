e3c1f2fd06317e865c7f06c15f3f93c6eda60e70
mako-agawa <mako.agawa@gmail.com>
2024-09-06 22:50:41 +0900

    commit

--- public/idChange.js
+++ public/idChange.js
@@ -4,7 +4,7 @@ const data = {
       "duration_minute": "30",
       "duration_second": "07",
       "tasks": [
-        {"title": "Ruby", "hours": "01", "minutes": "15", "seconds": "00"},
+        {"title": "Ruby today", "hours": "01", "minutes": "15", "seconds": "00"},
         {"title": "チームミーティング", "hours": "02", "minutes": "05", "seconds": "00"},
         {"title": "JavaScript", "hours": "01", "minutes": "05", "seconds": "00"},
         {"title": "React", "hours": "02", "minutes": "05", "seconds": "00"},
@@ -51,3 +51,50 @@ const data = {
       }
     }
   };
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    function recallTodayData() {
+      console.log('------------today-------------');
+      console.log(data.today.duration_hour);
+      console.log(data.today.duration_minute);
+
+      const content = document.getElementById("content");
+      if (content) {
+        content.innerHTML = "";
+
+        const timeParagraph = document.createElement("p");
+        timeParagraph.className = "report-totaltime";
+        timeParagraph.textContent = `Total: ${data.today.duration_hour}時間 ${data.today.duration_minute}分`;
+        content.appendChild(timeParagraph);
+
+        const taskList = document.createElement("ul");
+        taskList.className = "task-list";
+
+        const todayTasks = data.today.tasks;
+        todayTasks.forEach(task => {
+          const taskItem = document.createElement("li");
+          taskItem.className = "task-item";
+          
+          const taskTitle = document.createElement("p");
+          taskTitle.className = "task-title";
+          taskTitle.textContent = task.title;
+ 
+          const taskTime = document.createElement("p");
+          taskTime.className = "task-time";
+          taskTime.textContent = `${task.hours}時間 ${task.minutes}分 ${task.seconds}秒`;
+
+          taskItem.appendChild(taskTitle);
+          taskItem.appendChild(taskTime);
+          taskList.appendChild(taskItem);
+        });
+
+        content.appendChild(taskList);
+      }
+    }
+
+    document.getElementById("prev").addEventListener("click", recallTodayData);
+
+    
+  });
+
