470b813f4aaab5257d2917ad69a250043989f23e
Shota Nakamichi <s02178906@gmail.com>
2024-09-07 20:49:11 +0900

    saveをhtmlの日付に紐付けた

--- public/save.js
+++ public/save.js
@@ -3,12 +3,13 @@ document.getElementById("save-btn").addEventListener("click", function (event) {
   event.preventDefault();  // フォームのデフォルトの送信を防ぐ
 
   const reportForm = document.querySelector('.editable-area').innerText;
+  const targetDay = document.querySelector(".report-date").innerText;
   fetch('/save_report', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ description: reportForm }),
+    body: JSON.stringify({ description: reportForm, date: targetDay }),
   })
   .then(response => response.json())
   .then(data => {
@@ -17,7 +18,7 @@ document.getElementById("save-btn").addEventListener("click", function (event) {
       alert("本日もお疲れ様でした！！")
     } else {
       console.error('Failed to save report:', data.message);
-      alert("失敗しました")
+      alert("データの送信に失敗しました")
     }
   })
   .catch(error => {
